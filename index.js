861a4fab35d14e95f910e5beba8ebc375f48b322
Shubhanshu Tiwari <tiwarishubhanshu1071@gmail.com>
2024-09-22 16:23:40 +0530

    backend added

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bfhlRoutes = require('./routes/bfhl');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use('/bfhl', bfhlRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
